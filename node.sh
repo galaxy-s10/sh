e86beb0f1b45d50deeb9461081815a17fb7f1ef7
shuisheng <2274751790@qq.com>
2022-08-15 09:16:32 +0800

    feat: 更新脚本

--- node.sh
+++ node.sh
@@ -4,7 +4,7 @@
 # Email: 2274751790@qq.com
 # Github: https://github.com/galaxy-s10
 # Date: 2022-01-10 10:56:03
-# LastEditTime: 2022-04-08 12:39:09
+# LastEditTime: 2022-08-15 09:13:31
 # Description: 区分环境的node通用构建脚本
 ###
 
@@ -15,18 +15,29 @@ WORKSPACE=$3
 PORT=$4
 PUBLICDIR=/node
 
+echo 任务名:$JOBNAME
+echo 环境:$JOBNAME
+echo 工作区:$WORKSPACE
+echo 端口:$PORT
+
+echo 进入jenkins工作区:
+cd $WORKSPACE
+
+echo 将jenkins工作区的代码复制到node目录
+
 if [ $ENV != 'null' ]; then
     echo 当前环境:$ENV
     if [ -d $PUBLICDIR/$JOBNAME/$ENV ]; then
         echo "$PUBLICDIR/$JOBNAME/$ENV/目录已经存在,先删除它,然后再重新创建它"
         rm -rf $PUBLICDIR/$JOBNAME/$ENV/
         mkdir -p $PUBLICDIR/$JOBNAME/$ENV/
-        cp -r $WORKSPACE/* $PUBLICDIR/$JOBNAME/$ENV/
+        cp -r $(ls -A $WORKSPACE | grep -v .git | xargs) $PUBLICDIR/$JOBNAME/$ENV/
     else
         echo "$PUBLICDIR/$JOBNAME/$ENV/目录还没有,创建它"
         mkdir -p $PUBLICDIR/$JOBNAME/$ENV/
-        cp -r $WORKSPACE/* $PUBLICDIR/$JOBNAME/$ENV/
+        cp -r $(ls -A $WORKSPACE | grep -v .git | xargs) $PUBLICDIR/$JOBNAME/$ENV/
     fi
+    echo "执行$PUBLICDIR/$JOBNAME/$ENV/pm2.sh"
     sh $PUBLICDIR/$JOBNAME/$ENV/pm2.sh $JOBNAME $ENV $WORKSPACE $PORT
 else
     echo 当前环境是null
@@ -34,11 +45,12 @@ else
         echo "$PUBLICDIR/$JOBNAME/目录已经存在,先删除它,然后再重新创建它"
         rm -rf $PUBLICDIR/$JOBNAME/
         mkdir -p $PUBLICDIR/$JOBNAME/
-        cp -r $WORKSPACE/* $PUBLICDIR/$JOBNAME/
+        cp -r $(ls -A $WORKSPACE | grep -v .git | xargs) $PUBLICDIR/$JOBNAME/
     else
         echo "$PUBLICDIR/$JOBNAME/目录还没有,创建它"
         mkdir -p $PUBLICDIR/$JOBNAME/
-        cp -r $WORKSPACE/* $PUBLICDIR/$JOBNAME/
+        cp -r $(ls -A $WORKSPACE | grep -v .git | xargs) $PUBLICDIR/$JOBNAME/
     fi
+    echo "执行$PUBLICDIR/$JOBNAME/$ENV/pm2.sh"
     sh $PUBLICDIR/$JOBNAME/pm2.sh $JOBNAME $ENV $WORKSPACE $PORT
 fi
--- pm2.sh
+++ pm2.sh
@@ -4,31 +4,68 @@
 # Email: 2274751790@qq.com
 # Github: https://github.com/galaxy-s10
 # Date: 2022-01-10 17:50:30
-# LastEditTime: 2022-01-16 17:08:17
+# LastEditTime: 2022-08-15 09:15:01
 # Description: pm2维护脚本
 ###
 
-# 约定$1为任务名, $2为环境, $3为Jenkins工作区
+# 约定$1为任务名, $2为环境, $3为Jenkins工作区, $4为端口号
 JOBNAME=$1 # 注意: JOBNAME=$1,这个等号左右不能有空格！
 ENV=$2
 WORKSPACE=$3
+PORT=$4
 PUBLICDIR=/node
 
 if ! type pm2 >/dev/null 2>&1; then
-  echo pm2未安装,先全局安装pm2:
+  echo pm2未安装,先全局安装pm2
   npm i pm2 -g
 else
   echo pm2已安装
 fi
 
+if ! type yarn >/dev/null 2>&1; then
+  echo yarn未安装,先全局安装yarn
+  npm i yarn -g
+else
+  echo yarn已安装
+fi
+
 # 注意：要先进入项目所在的目录，然后再执行pm2命令!!!
 # 否则的话约等于在其他目录执行npm run dev,如果所在的目录没有package.json文件就会报错！
-cd $PUBLICDIR/$JOBNAME
+cd $PUBLICDIR/$JOBNAME/$ENV
+
+echo 删除node_modules:
+rm -rf node_modules
+
+echo 查看npm版本:
+npm -v
+
+echo 设置npm淘宝镜像:
+npm config set registry http://registry.npm.taobao.org/
+
+echo 查看当前npm镜像:
+npm get registry
+
+echo 查看yarn版本:
+yarn -v
+
+echo 设置yarn淘宝镜像:
+yarn config set registry https://registry.npm.taobao.org
+
+echo 查看当前yarn镜像:
+yarn config get registry
+
+echo 开始安装依赖:
+yarn install
 
 echo 删除旧的pm2服务:
-pm2 del $JOBNAME
+pm2 del $JOBNAME-$ENV-$PORT
 
-echo 使用pm2维护:
-pm2 start app.js --name $JOBNAME
+echo 使用pm2维护：
+# pm2 start ./src/index.ts --name $JOBNAME-$ENV --interpreter ./node_modules/.bin/nodemon
+# pm2 start ./src/index.ts --name $JOBNAME-$ENV --interpreter ./node_modules/.bin/ts-node
+# pm2 start --name $JOBNAME-$ENV ts-node -- -P tsconfig.json ./src/index.ts
+npx cross-env NODE_APP_RELEASE_PROJECT_NAME=$JOBNAME NODE_APP_RELEASE_PROJECT_ENV=$ENV NODE_APP_RELEASE_PROJECT_PORT=$PORT pm2 start --name $JOBNAME-$ENV-$PORT ts-node -- -P tsconfig.json ./src/index.ts
+pm2 save
 
-# pm2 start npm --name $JOBNAME -- run dev
+# echo 使用pm2维护：
+# pm2 start $PUBLICDIR/$JOBNAME/app.js --name $JOBNAME
